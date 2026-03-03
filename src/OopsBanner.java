f017cc014ff0dbdc7aa8723409f3466e54014cc1
irajanurudd <irajanirudd@gmail.com>
2026-03-03 10:34:44 +0530

    add uc1: print OOPS

--- src/OopsBanner.java
+++ src/OopsBanner.java
@@ -1,6 +1,7 @@
 public class OopsBanner{
     public static void main(String[] args){
-        System.out.println("Welcome to oops banner....");
+        // uc1
+        System.out.println("OOPS");
       
     }
 }
