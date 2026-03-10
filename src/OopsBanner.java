9f7d489e127e90f191404c3724c3db783ea634ec
irajanurudd <irajanirudd@gmail.com>
2026-03-10 10:08:41 +0530

    add uc6:print oops

--- src/OopsBanner.java
+++ src/OopsBanner.java
@@ -1,23 +1,64 @@
 public class OopsBanner {
 
+    // Method to return pattern of O
+    public static String getOPattern(int line) {
+        String[] O = {
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        };
+        return O[line];
+    }
+
+    // Method to return pattern of P
+    public static String getPPattern(int line) {
+        String[] P = {
+                " ***** ",
+                "*     *",
+                "*     *",
+                " ***** ",
+                "*      ",
+                "*      ",
+                "*      "
+        };
+        return P[line];
+    }
+
+    // Method to return pattern of S
+    public static String getSPattern(int line) {
+        String[] S = {
+                " ***** ",
+                "*      ",
+                "*      ",
+                " ***** ",
+                "      *",
+                "*     *",
+                " ***** "
+        };
+        return S[line];
+    }
+
     public static void main(String[] args) {
 
-        System.out.println("OOPS Banner App - UC5\n");
+        System.out.println("OOPS Banner App - UC6\n");
 
-        // UC5: Inline Array Initialization with String.join()
-        String[] banner = {
+        // Banner array populated using methods
+        String[] banner = new String[7];
 
-            String.join("   ", " ***** ", " ***** ", " ***** ", " ***** "),
-            String.join("   ", "*     *", "*     *", "*     *", "*      "),
-            String.join("   ", "*     *", "*     *", "*     *", "*      "),
-            String.join("   ", "*     *", "*     *", " ***** ", " ***** "),
-            String.join("   ", "*     *", "*     *", "*      ", "      *"),
-            String.join("   ", "*     *", "*     *", "*      ", "*     *"),
-            String.join("   ", " ***** ", " ***** ", "*      ", " ***** ")
+        for (int i = 0; i < 7; i++) {
+            banner[i] = String.join("   ",
+                    getOPattern(i),
+                    getOPattern(i),
+                    getPPattern(i),
+                    getSPattern(i)
+            );
+        }
 
-        };
-
-        // Enhanced For Loop to print banner
+        // Enhanced for loop to print
         for (String line : banner) {
             System.out.println(line);
         }
